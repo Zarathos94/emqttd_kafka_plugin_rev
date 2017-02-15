239c0d89fde5ba5073108346c755b7b87d0add5a
Zarathos94 <irfansaric94@gmail.com>
2017-02-15 18:59:21 +0100

    Changes new message types

--- src/emqttd_plugin_kafka_bridge.erl
+++ src/emqttd_plugin_kafka_bridge.erl
@@ -37,9 +37,11 @@
 %% Hooks functions
 -export([on_client_connected/3, on_client_disconnected/3]).
 
--export([on_client_subscribe/3, on_client_subscribe_after/3, on_client_unsubscribe/3]).
+-export([on_client_subscribe/4, on_client_unsubscribe/4]).
 
--export([on_message_publish/2, on_message_delivered/3, on_message_acked/3]).
+-export([on_session_created/3, on_session_subscribed/4, on_session_unsubscribed/4, on_session_terminated/4]).
+
+-export([on_message_publish/2, on_message_delivered/4, on_message_acked/4]).
 
 %% Called when the plugin application start
 load(Env) ->
@@ -47,8 +49,11 @@ load(Env) ->
     emqttd:hook('client.connected', fun ?MODULE:on_client_connected/3, [Env]),
     emqttd:hook('client.disconnected', fun ?MODULE:on_client_disconnected/3, [Env]),
     emqttd:hook('client.subscribe', fun ?MODULE:on_client_subscribe/3, [Env]),
-    emqttd:hook('client.subscribe.after', fun ?MODULE:on_client_subscribe_after/3, [Env]),
     emqttd:hook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/3, [Env]),
+    emqttd:hook('session.created', fun ?MODULE:on_session_created/3, [Env]),
+    emqttd:hook('session.subscribed', fun ?MODULE:on_session_subscribed/4, [Env]),
+    emqttd:hook('session.unsubscribed', fun ?MODULE:on_session_unsubscribed/4, [Env]),
+    emqttd:hook('session.terminated', fun ?MODULE:on_session_terminated/4, [Env]),
     emqttd:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]),
     emqttd:hook('message.delivered', fun ?MODULE:on_message_delivered/3, [Env]),
     emqttd:hook('message.acked', fun ?MODULE:on_message_acked/3, [Env]).
@@ -98,12 +103,7 @@ on_client_disconnected(Reason, ClientId, _Env) ->
 
 %% should retain TopicTable
 on_client_subscribe(ClientId, TopicTable, _Env) ->
-    io:format("client ~s will subscribe ~p~n", [ClientId, TopicTable]),
-    {ok, TopicTable}.
-   
-on_client_subscribe_after(ClientId, TopicTable, _Env) ->
     io:format("client ~s subscribed ~p~n", [ClientId, TopicTable]),
-    
     case TopicTable of
         [_|_] -> 
             %% If TopicTable list is not empty
@@ -123,7 +123,6 @@ on_client_subscribe_after(ClientId, TopicTable, _Env) ->
     end,
 
     {ok, TopicTable}.
-
 %%-----------client subscribed end----------------------------------------%%
 
 
@@ -179,6 +178,56 @@ on_message_publish(Message, _Env) ->
     ekaf:produce_async_batched(<<"broker_message">>, list_to_binary(Json)),
 
     {ok, Message}.
+
+on_session_created(ClientId, Username, _Env) ->
+    io:format("session(~s/~s) created.", [ClientId, Username])
+    Json = mochijson2:encode([
+        {type, <<"session_created">>},
+        {client_id, ClientId},
+        {username, Username},
+        {cluster_node, node()},
+        {ts, emqttd_time::now_to_secs(Timestamp)}
+    ]),
+    ekaf:produce_async_batched(<<"broker_message">>, list_to_binary(Json)).
+
+on_session_subscribed(ClientId, Username, {Topic, Opts}, _Env) ->
+    io:format("session(~s/~s) subscribed: ~p~n", [Username, ClientId, {Topic, Opts}]),
+    Json = mochijson2:encode([
+        {type, <<"session_created">>},
+        {client_id, ClientId},
+        {username, Username},
+        {topic, Topic},
+        {cluster_node, node()},
+        {ts, emqttd_time::now_to_secs(Timestamp)}
+    ]),
+    ekaf:produce_async_batched(<<"broker_message">>, list_to_binary(Json)),
+    {ok, {Topic, Opts}}.
+
+on_session_unsubscribed(ClientId, Username, {Topic, Opts}, _Env) ->
+    io:format("session(~s/~s) unsubscribed: ~p~n", [Username, ClientId, {Topic, Opts}]),
+    Json = mochijson2:encode([
+        {type, <<"session_unsubscribed">>},
+        {client_id, ClientId},
+        {username, Username},
+        {topic, Topic},
+        {cluster_node, node()},
+        {ts, emqttd_time::now_to_secs(Timestamp)}
+    ]),
+    ekaf:produce_async_batched(<<"broker_message">>, list_to_binary(Json)),
+    ok.
+
+on_session_terminated(ClientId, Username, Reason, _Env) ->
+    io:format("session(~s/~s) terminated: ~p.", [ClientId, Username, Reason]),
+    Json = mochijson2:encode([
+        {type, <<"session_terminated">>},
+        {client_id, ClientId},
+        {username, Username},
+        {reason, Reason},
+        {cluster_node, node()},
+        {ts, emqttd_time::now_to_secs(Timestamp)}
+    ]),
+    ekaf:produce_async_batched(<<"broker_message">>, list_to_binary(Json)),.
+
 
 %%-----------message delivered start--------------------------------------%%
 on_message_delivered(ClientId, Message, _Env) ->
