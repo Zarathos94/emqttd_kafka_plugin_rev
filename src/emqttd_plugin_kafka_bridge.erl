ed92d7d0c9de3dda96a1e5c94597f7664d1a3672
Irfan Saric <Irfan Saric>
2017-04-25 22:48:36 +0200

    UUID Check

--- src/emqttd_plugin_kafka_bridge.erl
+++ src/emqttd_plugin_kafka_bridge.erl
@@ -197,7 +197,7 @@ on_message_publish(Message, _Env) ->
     Payload = Message#mqtt_message.payload,
     %PacketId = Message#mqtt_message.pktid,
     %QoS = Message#mqtt_message.qos,
-    io:format("publish ~p ~n", [integer_to_list(MessageId, 8)]),
+    io:format("publish ~p ~n", [list_to_binary(integer_to_list(MessageId, 16))]),
     Json = mochijson2:encode([
         {type, <<"message_published">>},
         {client_id, ClientId},
