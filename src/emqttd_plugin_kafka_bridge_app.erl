391adf4eda9cb704192055982c2f4652da96baaf
Zarathos94 <irfansaric94@gmail.com>
2017-08-17 01:37:23 +0200

    Config attempt

--- src/emqttd_plugin_kafka_bridge_app.erl
+++ src/emqttd_plugin_kafka_bridge_app.erl
@@ -36,10 +36,10 @@
 start(_StartType, _StartArgs) ->
     {ok, Sup} = emqttd_plugin_kafka_bridge_sup:start_link(),
     %%emqttd_plugin_kafka_bridge:load(application:get_all_env()),
-    %%emqttd_plugin_kafka_bridge_config:register(),
+    emqttd_plugin_kafka_bridge_config:register(),
     {ok, Sup}.
 
 stop(_State) ->
-    %%emqttd_plugin_kafka_bridge_config_config:unregister(),
+    emqttd_plugin_kafka_bridge_config:unregister(),
     emqttd_plugin_kafka_bridge:unload().
 
