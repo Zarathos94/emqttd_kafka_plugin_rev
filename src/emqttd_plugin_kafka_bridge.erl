3d747ce585820ce3ff8e5dcb134de1843dac3e28
Zarathos94 <irfansaric94@gmail.com>
2017-08-17 14:09:56 +0200

    proplists removed

--- src/emqttd_plugin_kafka_bridge.erl
+++ src/emqttd_plugin_kafka_bridge.erl
@@ -352,11 +352,11 @@ on_message_acked(ClientId, Username, Message, _Env) ->
 
 rmq_init() ->
   %%{ok, Rmq} = application:get_env(emqttd_plugin_kafka_bridge, amqp_client),
-  Virtualhost = proplists:get_env(?APP, virtualhost, "/"),
-  Username = proplists:get_env(?APP, username),
-  Password = proplists:get_env(?APP, password),
-  RMQPort = proplists:get_env(?APP, port),
-  RMQHost = proplists:get_env(?APP, host),
+  Virtualhost = application:get_env(?APP, virtualhost, "/"),
+  Username = application:get_env(?APP, username),
+  Password = application:get_env(?APP, password),
+  RMQPort = application:get_env(?APP, port),
+  RMQHost = application:get_env(?APP, host),
   io:format("Trying to connect to:  ~p~n", [RMQHost]),
   {ok, Connection} = amqp_connection:start(#amqp_params_network{
     username = Username, password = Password, virtual_host = Virtualhost,
