a098637561f0d56d7750b3461fd69c54c968dab4
Zarathos94 <irfansaric94@gmail.com>
2017-08-17 12:22:10 +0200

    Fixed Loading

--- src/emqttd_plugin_kafka_bridge_app.erl
+++ src/emqttd_plugin_kafka_bridge_app.erl
@@ -36,6 +36,7 @@
 start(_StartType, _StartArgs) ->
     {ok, Sup} = emqttd_plugin_kafka_bridge_sup:start_link(),
     %%emqttd_plugin_kafka_bridge:load(application:get_all_env()),
+    emqttd_plugin_kafka_bridge:load(),
     emqttd_plugin_kafka_bridge_config:register(),
     {ok, Sup}.
 
